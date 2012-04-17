fa7768a926ddb158a88b03d91e44627ed7cee5e9
Drew Inglis <drew.inglis@gmail.com>
2012-04-16 20:08:01 -0400

    fix typo and update schema

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
-  validates :content, :presenct => true
+  validates :content, :presence => true
   validates :name, :presence => true
   validates :email, :presence => true, :format => { :with => /^[^@]+@[\w\.]+\.\w{2,4}$/ }
 end
